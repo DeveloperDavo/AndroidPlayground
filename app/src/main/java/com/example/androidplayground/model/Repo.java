d4be143847465578efe2156bd8b1b781c1c52f8f
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-01-23 19:30:56 +0100

    Update Repo to include name

--- app/src/main/java/com/example/androidplayground/model/Repo.java
+++ app/src/main/java/com/example/androidplayground/model/Repo.java
@@ -1,4 +1,20 @@
 package com.example.androidplayground.model;
 
 public class Repo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Repo{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
