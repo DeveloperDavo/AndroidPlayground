8ea573776a4e3c7180fe0f79f568bfe6a7569f70
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-01-28 18:38:59 +0100

    Complete test to check if DogsActivity has been launched

--- app/src/androidTest/java/com/example/androidplayground/home/HomeScreenTest.java
+++ app/src/androidTest/java/com/example/androidplayground/home/HomeScreenTest.java
@@ -1,36 +1,33 @@
 package com.example.androidplayground.home;
 
-import android.support.test.espresso.DataInteraction;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.androidplayground.R;
+import com.example.androidplayground.dogs.DogsActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.anything;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class HomeScreenTest {
 
     @Rule
-    public ActivityTestRule<HomeActivity> homeActivityRule =
-            new ActivityTestRule<>(HomeActivity.class);
+    public IntentsTestRule<HomeActivity> homeActivityRule =
+            new IntentsTestRule<>(HomeActivity.class);
 
     @Test
     public void shouldOpenDogsScreenOnClick() {
@@ -39,7 +36,7 @@ public class HomeScreenTest {
         onView(withId(R.id.btn_dogs))
                 .perform(click());
 
-        // TODO: check dogs screen is visible
+        intended(hasComponent("com.example.androidplayground.dogs.DogsActivity"));
     }
 
 }
